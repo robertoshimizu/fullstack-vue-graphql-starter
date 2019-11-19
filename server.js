f656de810c3dabc33115bd5a76d8f049aca32794
robertoshimizu <rshimizubr@gmail.com>
2019-11-19 16:21:04 -0300

    Mutation, Query and first input to MongoDb created

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
 const mongoose = require('mongoose');
+
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname,'typeDefs.gql');
+const typeDefs = fs.readFileSync(filePath,'utf-8');
+const resolvers = require('./resolvers');
 
 require('dotenv').config({path: 'variables.env'});
 const User = require('./models/User');
@@ -14,6 +21,7 @@ mongoose
 
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
   context:{
     User,
     Post
