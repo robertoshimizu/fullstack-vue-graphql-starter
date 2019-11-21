c3ae7eed27959a1badb0ea60bebb0f7ab832b142
robertoshimizu <rshimizubr@gmail.com>
2019-11-21 10:26:02 -0300

    Added Post Mutation

--- server.js
+++ server.js
@@ -4,20 +4,26 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
 
+// Import typeDefs and resolvers
+
 const filePath = path.join(__dirname,'typeDefs.gql');
 const typeDefs = fs.readFileSync(filePath,'utf-8');
 const resolvers = require('./resolvers');
 
+// Import Environment Variables and Mongoose Models
+
 require('dotenv').config({path: 'variables.env'});
 const User = require('./models/User');
 const Post = require('./models/Post');
+
+// Connect to MongoDB database
 
 mongoose
   .connect(process.env.MONGO_URI,{ useNewUrlParser: true })
   .then(()=>console.log("MongoDB connected"))
   .catch(err=>console.log(err))
 
-// TypeDefs will be descripted in another file: typeDefs.gql
+// Create Apollo/GraphQL Server using typeDefs, resolvers and context objects
 
 const server = new ApolloServer({
   typeDefs,
@@ -27,6 +33,8 @@ const server = new ApolloServer({
     Post
   }
 });
+
+// Listening on port 4000
 
 server.listen().then(({ url }) => {
   console.log(`Server listening on ${url}`);
